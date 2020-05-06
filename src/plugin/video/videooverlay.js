9d1692669669748fac566a61b0dd1c04bf510616
Brad Hards <bradh@frogmouth.net>
2020-05-06 14:29:57 +1000

    Disable overlay, broken

--- src/plugin/video/videooverlay.js
+++ src/plugin/video/videooverlay.js
@@ -8,8 +8,10 @@ var VideoJSPlugin = videojs.getPlugin('plugin');
 var VideoOverlay = videojs.extend(VideoJSPlugin, {
 
   // el: {},
+  /*
   constructor: function (player, options) {
     VideoJSPlugin.call(this, player, options);
+
     this.el = {};
 
     if (options.customClass) {
@@ -21,10 +23,10 @@ var VideoOverlay = videojs.extend(VideoJSPlugin, {
         className: `vjs-overlay-item`
       });
       this.el.innerText = "QF776";
-      /* TODO: some of this styling could move to CSS */
+      // TODO: some of this styling could move to CSS
       this.el.style.position = "absolute";
       this.el.style.color = "white";
-      /* TODO: this needs to be derived from the on-screen track position */
+      // TODO: this needs to be derived from the on-screen track position
       this.el.style.top = "150px";
       this.el.style.left = "200px";
       player.el().appendChild(this.el);
@@ -37,6 +39,7 @@ var VideoOverlay = videojs.extend(VideoJSPlugin, {
     this.el.style.top = x + "px";
     this.el.style.left = y + "px";
   }
+  */
 });
 videojs.registerPlugin('overlayPlugin', VideoOverlay);
 
